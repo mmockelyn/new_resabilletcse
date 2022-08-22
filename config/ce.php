5427e8180ab3d9315e2009f011582ee8b56c96c6
Mockelyn Maxime <trainznation@gmail.com>
2022-08-22 03:01:04 +0200

    FX220820220300
    - Correct Key Obscurator

--- config/ce.php
+++ config/ce.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'ce_id' => '8430fb82-768d-4f80-9f19-0b073c5d7741',
+    'ce_id' => env("CE_ID"),
     'ce_name' => 'RESABILLETCSE',
-    'secret_key' => 'SECRET',
+    'secret_key' => env('CE_SECRET'),
     'signature' => sha1(config('ce.ce_id')+config('ce.secret_key')),
-    'tradedoubler_client_id' => '12d237f8-660d-3e34-9a12-970d4b96b38c',
-    'tradedoubler_client_secret' => 'cdd44f77163d8a7f',
+    'tradedoubler_client_id' => env("TRADEDOUBLER_CLIENT_ID"),
+    'tradedoubler_client_secret' => env("TRADEDOUBLER_CLIENT_SECRET"),
 ];
