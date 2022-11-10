3e6e1130c01cf3b80168559036bfafec1e4f2f76
mmockelyn <trainznation@gmail.com>
2022-11-10 17:15:36 +0100

    Co

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,15 +15,12 @@ class TestController extends Controller
      */
     public function code()
     {
-        /*try {
+        try {
             $client = new Api('https://webservices-test.reducce.fr/Partenaire.svc?wsdl', ['cache_wsdl' => WSDL_CACHE_NONE]);
         } catch (\SoapFault $e) {
             dd($e->getMessage());
         }
 
-        dd($client->GET_CATALOGUES(['partenaire_id' => config('ce.ce_id')]));*/
-
-        $client = new Program();
-        dd($client->list()->items);
+        dd($client->GET_CATALOGUES(['partenaire_id' => config('ce.ce_id')]));
     }
 }
